817789c815bd689b167a271fa99e2f245535b38e
Vivek Kumar <choudhary.vivek18@gmail.com>
2022-01-09 18:08:23 +0530

    different way to create components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,16 @@
-function HomePage(){
-    return (
-        <main className="px-3">
-            <h1>Cover your page.</h1>
-            <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-            <p className="lead">
-            <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-            </p>
-        </main>
-    );
-}
+import React from "react";
 
-export default HomePage;
+class HomePage extends React.Component{
+    render(){
+        return (
+            <main className="px-3">
+                <h1>Cover your page.</h1>
+                <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                <p className="lead">
+                <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+                </p>
+            </main>
+        );
+    }
+}
+ export default HomePage;
